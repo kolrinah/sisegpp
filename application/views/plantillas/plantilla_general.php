84a0770d0f5c5d73f3020488bd382999ad090c3a
kolrinah <kolrinah@gmail.com>
2013-11-14 20:17:14 -0430

    Revert "Sistema de Seguimiento de Proyecots"
    This reverts commit 1ee3bbce53f0af89ced356084ce1cb3879fcc118.

--- application/views/plantillas/plantilla_general.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('Sin Acceso Directo al Script');
-    $this->load->view('plantillas/cabecera');
-    $this->load->view('plantillas/encabezado');
-    $this->load->view('plantillas/menu');
-    $this->load->view($contenido);
-    $this->load->view('plantillas/pie_pagina');
-?>
